6958f34ecd64644124653bef5eaa455e884a7e05
demonyangyue <demonyangyue@gmail.com>
2014-05-14 15:42:20 +0800

    add user management

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#mail server configuration
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :authentication => "plain",
+   :user_name => ENV["GMAIL_USERNAME"],
+   :password => ENV["GMAIL_PASSWORD"],
+   :enable_starttls_auto => true,
+ }
